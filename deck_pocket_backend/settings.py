2447bc95d69420d89521422ee86eeb0bfcb36183
Luis Parada <luis.parada@a3sec.com>
2020-05-10 22:07:48 +0200

    Allow Headers

--- deck_pocket_backend/settings.py
+++ deck_pocket_backend/settings.py
@@ -158,7 +158,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.join(BASE_DIR), "static")
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'firebase-user',
 ]

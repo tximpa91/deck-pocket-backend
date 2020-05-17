f152919dd56b07b4c44b21f7b8f645e66f8ab2aa
Luis Parada <luis.parada@a3sec.com>
2020-05-17 12:30:38 +0200

    first

--- deck_pocket/graphql_fields/custom_fields.py
+++ deck_pocket/graphql_fields/custom_fields.py
@@ -4,3 +4,11 @@ import graphene
 class DeckDictionary(graphene.InputObjectType):
     card_id = graphene.String()
     have_it = graphene.Boolean()
+
+
+def first(queryset, limit):
+    limit_query = limit.get('first')
+    if limit_query:
+        return queryset[:limit_query]
+    else:
+        return queryset
--- deck_pocket/graphql_schema/global_schema.py
+++ deck_pocket/graphql_schema/global_schema.py
@@ -4,37 +4,38 @@ from .deck.deck_schema import DeckSchema
 from .whishlist.whishlist_schema import WhishlistSchema
 from .mycards.mycards_schema import MyCardSchema
 from deck_pocket.models import Card, Deck, Whishlist, MyCards
+from deck_pocket.graphql_fields.custom_fields import first
 
 
 class Query(graphene.ObjectType):
-    all_cards = graphene.List(CardSchema, deck_name=graphene.String())
-    card = graphene.List(CardSchema, card_name=graphene.String(), distinct=graphene.Boolean())
-    decks = graphene.List(DeckSchema, deck_name=graphene.String())
-    whishlist = graphene.List(WhishlistSchema)
-    my_cards = graphene.List(MyCardSchema)
+    all_cards = graphene.List(CardSchema, deck_name=graphene.String(), first=graphene.Int)
+    card = graphene.List(CardSchema, card_name=graphene.String(), distinct=graphene.Boolean(), first=graphene.Int)
+    decks = graphene.List(DeckSchema, deck_name=graphene.String(), first=graphene.Int)
+    whishlist = graphene.List(WhishlistSchema, first=graphene.Int)
+    my_cards = graphene.List(MyCardSchema, first=graphene.Int)
 
     def resolve_all_cards(self, info):
-        return Card.objects.all()[:10]
+        return Card.objects.all()
 
     def resolve_card(self, info, card_name, **kwargs):
         distinct = kwargs.get('distinct')
         queryset = Card.objects.filter(name__icontains=card_name)
         if distinct:
             return queryset.distinct('name')
-        return queryset
+        return first(queryset)
 
     def resolve_decks(self, info, **kwargs):
         user = kwargs.pop('user')
         deck_name = kwargs.get('deck_name')
         if deck_name:
-            return Deck.objects.filter(name__icontains=deck_name, user_deck=user)
+            return first(Deck.objects.filter(name__icontains=deck_name, user_deck=user), kwargs)
         else:
-            return Deck.objects.filter(user_deck=user)
+            return first(Deck.objects.filter(user_deck=user), kwargs)
 
     def resolve_whishlist(self, info, **kwargs):
         user = kwargs.pop('user')
-        return Whishlist.objects.filter(user_whishlist=user)
+        return first(Whishlist.objects.filter(user_whishlist=user), kwargs)
 
     def resolve_my_cards(self, info, **kwargs):
         user = kwargs.pop('user')
-        return MyCards.objects.filter(user_cards=user)
+        return first(MyCards.objects.filter(user_cards=user), kwargs)

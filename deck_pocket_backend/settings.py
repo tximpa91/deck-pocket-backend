e56e8bd62e38e9dced08a07b2cc9fd03416a365f
Luis Parada <luis.parada@a3sec.com>
2020-05-17 22:55:17 +0200

    Second Alpha

--- deck_pocket_backend/settings.py
+++ deck_pocket_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9uox7#=-+mn@(uh^ehzpa$*ylbb)g#7xs6dlee1y5+n%(d7azw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

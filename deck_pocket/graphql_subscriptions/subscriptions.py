ba6e0efc16609c9987336d52c79993ff521ab2a4
Luis Parada <luis.parada@a3sec.com>
2020-06-24 00:21:21 +0200

    trouble with signals

--- deck_pocket/graphql_subscriptions/subscriptions.py
+++ deck_pocket/graphql_subscriptions/subscriptions.py
@@ -29,7 +29,7 @@ class DeckUpdatedSubscription(graphene.ObjectType):
         ).map(lambda event: event.instance)
 
 
-class Subscription(DeckUpdatedSubscription, DeckCreatedSubscription):
+class Subscription(DeckUpdatedSubscription):
     hello = graphene.String()
 
     def resolve_hello(root, info, **kwargs):

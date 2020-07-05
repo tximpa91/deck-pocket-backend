e6f6b14879b74f20d9e1cbc5fc6f33ae299d3504
Luis Parada <luis.parada@a3sec.com>
2020-07-05 21:57:34 +0200

    remove logger

--- deck_pocket/management/commands/create_wishlist.py
+++ deck_pocket/management/commands/create_wishlist.py
@@ -12,12 +12,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         decks = Deck.objects.all()
         WishList.objects.all().delete()
-
-        self.stderr.write(str(len(decks)))
         for deck in decks:
             wish_list = WishList()
             wish_list.save()
             wish_list.user_wish_list = deck.user_deck
             wish_list.deck_id.add(deck)
             wish_list.save()
-        self.stderr.write(str(WishList.objects.all()))

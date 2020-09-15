382b27d4e3ed3b7001f6ce0e6be67197f18d501e
Luis Parada <luis.parada@a3sec.com>
2020-09-15 10:17:32 +0200

    update lang

--- deck_pocket/management/commands/update_lang.py
+++ deck_pocket/management/commands/update_lang.py
@@ -13,14 +13,19 @@ class Command(BaseCommand):
         try:
             json_file = open('/Users/luisparada/Downloads/all-cards-20200914091746.json', 'r')
             data = json.load(json_file)
-            for data_d in data:
-                card = Card.objects.filter(id=data_d.get('id'))
-                if card.count() > 0:
-                    card = card[0]
-                    card.lang = data_d.get('lang')
-                    card.save()
 
-            self.stdout.write(str("Done"))
+            cards_pending = Card.objects.filter(lang__isnull=True).values_list('id', flat=True)
+            self.stdout.write(str(len(data)))
+            filtered_data = [
+                card for card in data if card.get('id') in cards_pending
+            ]
+            self.stdout.write(str(f"filtered data: {len(data)}"))
+            for card in filtered_data:
+                card_to_update = Card.objects.get(id=card.get('id'))
+                card_to_update.lang = card.get('lang')
+                card_to_update.save()
+
+            self.stdout.write(str(len("done")))
 
         except Exception as error:
             self.stderr.write(str(error))

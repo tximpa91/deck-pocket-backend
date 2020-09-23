136750438f543a6e730dd3b794fc934b01e92390
Luis Parada <luis.parada@a3sec.com>
2020-09-23 02:40:23 +0200

    load cards

--- deck_pocket/management/commands/calculate_real_type.py
+++ deck_pocket/management/commands/calculate_real_type.py
@@ -6,16 +6,16 @@ from deck_pocket.models import Card, WishList, MyCards, Deck, CardTypes
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        artifacts = Card.objects.filter(type_line__icontains=CardTypes.ARTIFACT.value).\
+        artifacts = Card.objects.filter(type_line__icontains=CardTypes.ARTIFACT.value, card_type__isnull=True ).\
             exclude(type_line__icontains=CardTypes.CREATURE.value). \
             exclude(type_line__icontains=CardTypes.LAND.value)
-        enchantment = Card.objects.filter(type_line__icontains=CardTypes.ENCHANTMENT.value).\
+        enchantment = Card.objects.filter(type_line__icontains=CardTypes.ENCHANTMENT.value, card_type__isnull=True).\
             exclude(type_line__icontains=CardTypes.CREATURE.value)
-        creature = Card.objects.filter(type_line__icontains=CardTypes.CREATURE.value)
-        instant = Card.objects.filter(type_line__icontains=CardTypes.INSTANT.value)
-        sorcery = Card.objects.filter(type_line__icontains=CardTypes.SORCERY.value)
-        land = Card.objects.filter(type_line__icontains=CardTypes.LAND.value)
-        planeswalker = Card.objects.filter(type_line__icontains=CardTypes.PLANESWALKER.value)
+        creature = Card.objects.filter(type_line__icontains=CardTypes.CREATURE.value, card_type__isnull=True)
+        instant = Card.objects.filter(type_line__icontains=CardTypes.INSTANT.value, card_type__isnull=True)
+        sorcery = Card.objects.filter(type_line__icontains=CardTypes.SORCERY.value, card_type__isnull=True)
+        land = Card.objects.filter(type_line__icontains=CardTypes.LAND.value, card_type__isnull=True)
+        planeswalker = Card.objects.filter(type_line__icontains=CardTypes.PLANESWALKER.value, card_type__isnull=True)
 
         artifacts.update(card_type=CardTypes.ARTIFACT.value)
         enchantment.update(card_type=CardTypes.ENCHANTMENT.value)
